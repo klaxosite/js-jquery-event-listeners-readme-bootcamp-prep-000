5766770897dd8ed8ca4dd2d18eaad9457f0fa792
Jastien Sandhu <superjastin@gmail.com>
2020-05-16 04:05:43 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -17,8 +17,8 @@ function pressIt(){
 }
 function submitIt(){
   $("form").on("submit", function(){
-    alert("Your form is going to be submitted now.")
-  })
+    alert("Your form is going to be submitted now.");
+  });
 }
 $(document).ready(function(){
 
